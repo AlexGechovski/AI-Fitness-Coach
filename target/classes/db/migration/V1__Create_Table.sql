40d278bb18c48165370f9a9c8e41eca8c83a30cc
AlexGechovski <aleksg00077@gmail.com>
2023-07-06 15:45:04 +0300

    Set up

---
+++ target/classes/db/migration/V1__Create_Table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    registration_date DATE NOT NULL
+    );
